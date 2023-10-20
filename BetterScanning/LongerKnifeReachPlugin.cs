b9623698894e34bf16ac60e2f941bb8b9dc6de64
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2023-10-20 13:23:45 +0200

    Implemented longer knife reach

---
+++ BetterScanning/LongerKnifeReachPlugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+
+using HarmonyLib;
+
+namespace VS.Subnautica.LongerKnifeReach
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class LongerKnifeReachPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.vs.subnautica.longerknifereach";
+        private const string PluginName = "Long Knife Reach";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+        public static ManualLogSource Log;
+
+        private void SendLoadedPluginMessage()
+        {
+            Logger.LogInfo("Successfully loaded {PluginName} (Version {VersionString})!");
+            Logger.LogInfo("> Thank you very much for using my mod! - Vip");
+        }
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+            SendLoadedPluginMessage();
+
+            Log = Logger;
+        }
+    }
+}
---
+++ BetterScanning/Patches/KnifePatcher.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace VS.Subnautica.LongerKnifeReach.Patches
+{
+    [HarmonyPatch(typeof(Knife))]
+    internal class KnifePatcher
+    {
+        public static float MAXIMUM_ATTACK_DISTANCE = 1.95F;
+
+        [HarmonyPatch(nameof(Knife.OnToolUseAnim))]
+        [HarmonyPrefix]
+        public static void OnToolUseAnim_Prefix(Knife __instance)
+        {
+            __instance.attackDist = MAXIMUM_ATTACK_DISTANCE;
+        }
+    }
+}
