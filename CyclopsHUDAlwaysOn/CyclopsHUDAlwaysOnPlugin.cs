c3234fe1a5d585f760fcfe672884504237c43bf2
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2023-10-23 09:29:24 +0200

    Implemented Cyclops HUD Always On

---
+++ CyclopsHUDAlwaysOn/CyclopsHUDAlwaysOnPlugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx.Logging;
+using BepInEx;
+
+using HarmonyLib;
+
+namespace VS.Subnautica.CyclopsHUDAlwaysOn
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    public class CyclopsHUDAlwaysOnPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.vs.subnautica.cyclopshudalwayson";
+        private const string PluginName = "Cyclops HUD Always ON";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+        public static ManualLogSource Log;
+
+        private void SendLoadedPluginMessage()
+        {
+            Logger.LogInfo($"Successfully loaded {PluginName} (Version {VersionString})!");
+            Logger.LogInfo($"> Thank you very much for using my mod! - Vip");
+        }
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+            SendLoadedPluginMessage();
+
+            Log = Logger;
+        }
+    }
+}
---
+++ CyclopsHUDAlwaysOn/Patches/CyclopsHelmHUDManagerPatcher.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace VS.Subnautica.CyclopsHUDAlwaysOn.Patches
+{
+    [HarmonyPatch(typeof(CyclopsHelmHUDManager))]
+    internal class CyclopsHelmHUDManagerPatcher
+    {
+        /// <summary>
+        /// Makes sure the HUD gets enabled when the cyclops starts.
+        /// </summary>
+        [HarmonyPatch(nameof(CyclopsHelmHUDManager.Start))]
+        [HarmonyPostfix]
+        private static void Start_Postfix(CyclopsHelmHUDManager __instance)
+        {
+            __instance.hudActive = true;
+            
+            if (__instance.motorMode.engineOn)
+            {
+                __instance.engineToggleAnimator.SetTrigger("EngineOn");
+                return;
+            }
+
+            __instance.engineToggleAnimator.SetTrigger("EngineOff");
+        }
+
+        /// <summary>
+        /// Makes sure the HUD stays active when the user stops piloting it.
+        /// </summary>
+        [HarmonyPatch(nameof(CyclopsHelmHUDManager.StopPiloting))]
+        [HarmonyPostfix]
+        private static void StopPiloting_Postfix(CyclopsHelmHUDManager __instance)
+        {
+            __instance.hudActive = true;
+            __instance.hornObject.SetActive(true);
+        }
+    }
+}
